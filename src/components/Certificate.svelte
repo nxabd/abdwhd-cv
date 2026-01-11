e81b6c70c1e37974250979891fa174f160c4eb3f
nxabd <abdwahid8813@gmail.com>
2026-01-11 15:57:08 +0700

    fixing caraosel size

--- src/components/Certificate.svelte
+++ src/components/Certificate.svelte
@@ -5,9 +5,9 @@
 
 <section class="mt-10">
     <h1 class="text-2xl font-semibold">Certificate</h1>
-    <Carousel  particlesToShow={3} particlesToScroll={2}>
+    <Carousel  particlesToShow={3} particlesToScroll={2} dots={false} arrows={false}>
         {#each certif as img}
-            <img src={img} alt="" class="m-1 object-cover mt-4">
+            <img src={img} alt="" class="m-1 object-cover mt-2">
         {/each}
     </Carousel>
      
